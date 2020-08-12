8248ef2eac6dda269a185465e72bbf9a1b358aaf
King Lemming <kinglemming@gmail.com>
2020-08-12 02:18:07 -0700

    Add Fletcher trades to Archer's Paradox.
    Signed-off-by: King Lemming <kinglemming@gmail.com>

--- ThermalCore/src/main/java/cofh/thermal/core/util/managers/device/TreeExtractorManager.java
+++ ThermalCore/src/main/java/cofh/thermal/core/util/managers/device/TreeExtractorManager.java
@@ -4,10 +4,8 @@ import cofh.lib.util.ComparableItemStack;
 import cofh.thermal.core.util.managers.IManager;
 import com.google.common.collect.HashMultimap;
 import com.google.common.collect.SetMultimap;
-import it.unimi.dsi.fastutil.objects.Object2FloatOpenHashMap;
 import it.unimi.dsi.fastutil.objects.Object2ObjectOpenHashMap;
 import net.minecraft.block.BlockState;
-import net.minecraft.item.ItemStack;
 import net.minecraft.item.crafting.RecipeManager;
 import net.minecraftforge.fluids.FluidStack;
 
@@ -20,15 +18,18 @@ public class TreeExtractorManager implements IManager {
     protected Map<BlockState, FluidStack> trunkMap = new Object2ObjectOpenHashMap<>();
     protected SetMultimap<BlockState, BlockState> leafMap = HashMultimap.create();
 
-    protected Map<ComparableItemStack, Float> fertilizerMap = new Object2FloatOpenHashMap<>();
-
-    public float getFertilizerMultiplier(ItemStack stack) {
-
-        if (stack.isEmpty()) {
-            return 0;
-        }
-        return fertilizerMap.get(new ComparableItemStack(stack));
-    }
+    // TODO: Proper Catalyzing
+    //    protected Object2FloatMap<IItemProvider> fertilizerMap = new Object2FloatOpenHashMap<>();
+    //
+    //    public float getFertilizerMultiplier(ItemStack stack) {
+    //
+    //        return fertilizerMap.getFloat(stack.getItem());
+    //    }
+    //
+    //    public void addFertilizer(ItemStack fertilizer, float multiplier) {
+    //
+    //        fertilizerMap.put(fertilizer.getItem(), multiplier);
+    //    }
 
     // region IManager
     @Override
